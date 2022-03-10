43f1a2498a14e1204c7adbacceffe6ac66186e27
FisyonGames <fisyon.games@gmail.com>
2022-03-10 21:59:46 +0300

    Fixex Collate Script, Added EditorCoroutines

--- ArrowFestTutorial/Assets/ArrowFestTutorial/Scripts/ArrowContainer.cs
+++ ArrowFestTutorial/Assets/ArrowFestTutorial/Scripts/ArrowContainer.cs
@@ -3,16 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
+
 public class ArrowContainer : MonoBehaviour
 {
-
     [SerializeField] private Transform arrowPrefab;
     [SerializeField] private List<Transform> arrows = new List<Transform>();
     [SerializeField] private GameController gameController;
+    [SerializeField] [Range(137.0f, 138.0f)] private float degree = 137.5231367f;   // Farklı Dağıtma Açıları(Divergence Angle): 137.3°, 137.3692546°, 137.4038819°, 137.4731367°, 137.5077641°, 137.5231367°, 137.553882°, 137.5692547°, 137.6° 
+    [SerializeField] [Range(0.05f, 1.0f)] private float dotScale = 0.1f; // c
+
     private int arrowCount;
-    private float c = 5.25f;
-    private float radiusScale = 0.0001f;
-    
 
     public int ArrowCount { get { return arrowCount; } set { arrowCount = value; }}
     public List<Transform> Arrows { get { return arrows; }}
@@ -65,12 +65,12 @@ public class ArrowContainer : MonoBehaviour
 
     void CollateArrow()
     {
-        for(int i = 1; i <= arrows.Count; i++)
+        for (int i = 0; i < arrows.Count; i++)
         {
-            float phi = c * Mathf.Sqrt(i);
-            float r = i * 137.3f * radiusScale;
-            Vector3 point = new Vector3( Mathf.Cos(phi) * r , Mathf.Sin(phi) * r , 0 );
-            arrows[i-1].localPosition = point;
+            float angle = i * (degree * Mathf.Deg2Rad);     // angle(φ) = n * 137.5°
+            float r = dotScale * Mathf.Sqrt(i);             // radius(r) = c * √n
+            Vector3 point = new Vector3( Mathf.Cos(angle) * r , Mathf.Sin(angle) * r , 0 );
+            arrows[i].localPosition = point;
         }
     }
 
